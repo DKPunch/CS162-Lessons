5bd77651db490e3cb346446226f1ea1129af78b6
Joseph Schell <alejandro1one@hotmail.com>
2017-01-03 17:57:32 -0800

    Update main.cpp

--- Linked Lists/main.cpp
+++ Linked Lists/main.cpp
@@ -8,6 +8,7 @@ public:
 	Node * next;
 };
 
+// Function Prototypes
 bool isEmpty(Node * head);
 char menu();
 void insertAsFirstElement(Node *& head, Node *& last, int number);
@@ -18,33 +19,37 @@ void showList(Node * current);
 int main() {
 	Node * head = nullptr;
 	Node * last = nullptr;
-	int number;
+	int num;
 	char reply;
 
-	menu();
+	cout << "Example Linked List program.\n" << endl;
 
-	switch (tolower(reply)) {
-	case '1':
-		cout << "Please enter a number: ";
-		cin >> number;
-		insert(head, last, number);
-		break;
-	case '2':
-		addCourse();
-		break;
-	case '3':
-		deleteCourse();
-		break;
-	case '4':
-		return 0;
-		break;
-	default:
-		cout << "Error, invalid input.\n" << std::endl;
-		break;
-	}
+	do {
+		reply = menu();
+
+		switch (reply) {
+		case '1':
+			cout << "Please enter a number: ";
+			cin >> num;
+			insert(head, last, num);
+			break;
+		case '2':
+			remove(head, last);
+			break;
+		case '3':
+			showList(head);
+			break;
+		case '4':
+			cout << "Terminating Program...\n" << endl;
+			break;
+		default:
+			cout << "Invalid input. Please try again.\n" << endl;
+			break;
+		}
+	} while (reply != '4');
+
 	system("pause");
 	return 0;
-
 }
 
 bool isEmpty(Node * head) {
@@ -60,18 +65,16 @@ bool isEmpty(Node * head) {
 char menu() {
 	char choice;
 
-	cout << "Welcome to the Course Transcript and GPA program." << endl;
-
-	do {    // Welcome Screen
-		cout << "What would you like to do:\n"
-			<< "1: Add item from list.\n"
-			<< "2: Remove item from list.\n"
-			<< "3: Display list.\n"
-			<< "4: Quit program." << endl;
-		cout << "--> ";
-		cin >> choice;
-
-	} while (toupper(choice) != '4');
+	// Welcome Screen
+	cout << "Main Menu:" << endl;
+	cout << "What would you like to do...\n"
+		<< "1: Add item from list.\n"
+		<< "2: Remove item from list.\n"
+		<< "3: Display list.\n"
+		<< "4: Quit program." << endl;
+	cout << "--> ";
+	cin >> choice;
+	return choice;
 }
 
 void insertAsFirstElement(Node *& head, Node *& last, int number) {
@@ -94,36 +97,37 @@ void insert(Node *& head, Node *& last, int number) {
 		last->next = temp; // set previous last value to temp
 		last = temp; // update last to point to temp
 	}
-
+	cout << "Successfully added " << number << " to the list.\n" << endl;
 }
 
 void remove(Node *& head, Node *& last) {
 	if (isEmpty(head)) {
-		cout << "\nThe list is empty, cannot remove any nodes!!" << endl;
+		cout << "\nThe list is empty, cannot remove any nodes!!\n" << endl;
 	}
 	else if (head == last) {
-		delete head;
-		head = nullptr;
-		last = nullptr;
+		delete head;  // delete dynamic memory
+		head = nullptr; // make sure pointer points to null
+		last = nullptr; // make sure pointer points to null
+		cout << "Successfully removed the first node from the list.\n" << endl;
 	}
 	else {
-		Node * temp = head;
-		head = head->next;
-		delete temp;
+		Node * temp = head; // temp ptr set to head
+		head = head->next; // set head ptr to point at the data stored at next
+		delete temp; // delete dynamic memory stored at temp
+		cout << "Successfully removed the first node from the list.\n" << endl;
 	}
 }
 
 void showList(Node * current) {
 	if (isEmpty(current)) {
-		cout << "\nThe list is empty." << endl;
+		cout << "\nThe list is empty.\n" << endl;
 	}
 	else {
 		cout << "\nThe list contains: " << endl;
-		while (current != nullptr) {
-			cout << current->number << endl;
-			current = current->next;
+		while (current != nullptr) { // while not at the end of the list
+			cout << current->number << endl; // print data stored at node
+			current = current->next; // make current ptr point to next node
 		}
+		cout << "\n";
 	}
-
-
 }
